eb8396e2b18b5f706c055a3820d8a40a248f3025
Mwaka Ambrose <ambrosemwaka@gmail.com>
2023-03-05 15:13:00 +0300

    adds some backend sanity tests

--- api/tests/Feature/UserWeatherApiTest.php
+++ api/tests/Feature/UserWeatherApiTest.php
@@ -35,18 +35,13 @@ class UserWeatherApiTest extends TestCase
             ]);
     }
 
-    public function test_can_fetch_users_with_their_current_weather_data_and_weather_forecast_data()
+    public function test_can_fetch_users_with_their_weather_data_and_weather_forecast_data()
     {
         User::factory(count: 2)->create();
         Weather::factory(count: 20)->create();
-
         $user = User::inRandomOrder()->first();
-
-        $current_weather = $user->weather()->whereType(Weather::TYPES[0])->first();
         $weather_forecast = $user->weather()->whereType(Weather::TYPES[1])->get();
-
-        dd($current_weather->getAttributes(), $weather_forecast->toArray());
-
+        
         $this->get(uri: "/api/v1/users/{$user->id}")
             ->assertStatus(200)
             ->assertJsonStructure(structure: [
@@ -66,7 +61,6 @@ class UserWeatherApiTest extends TestCase
                 "data" => [
                     "user" => $user->toArray(),
                     "weather" => [
-                        "current" => $current_weather->getAttributes(),
                         "forecasts" => $weather_forecast->toArray(),
                     ],
                 ]
---
+++ api/tests/Unit/WeatherApiServiceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Services\WeatherApiService;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class WeatherApiServiceTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_getUsersCurrentWeather_fetch_user_weather_data_and_saves_it()
+    {
+        $user = User::factory()->create();
+        WeatherApiService::getUsersCurrentWeather($user);
+        $this->assertDatabaseHas('weather', [
+            'user_id' => $user->id,
+            'type' => 'current',
+        ]);
+    }
+
+    public function test_getUsers5DayWeatherForecast_fetch_user_weather_data_and_saves_it()
+    {
+        $user = User::factory()->create();
+        WeatherApiService::getUsers5DayWeatherForecast($user);
+        $this->assertDatabaseHas('weather', [
+            'user_id' => $user->id,
+            'type' => 'forecast',
+        ]);
+    }
+}
